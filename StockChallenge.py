e32557de1ea71a049c006cf0e796e9c8e7269b2a
Bizzy <elizabeth.lachat@gmail.com>
2023-08-16 23:11:15 -0400

    Update to if/else block to remove an extra unnecessary else statement

--- StockChallenge.py
+++ StockChallenge.py
@@ -6,8 +6,7 @@ def calculateMaxProfit(prices):
     for price in prices:
         if price < minPrice:
             minPrice = price
-        else:
-            if price - minPrice > profit:
-                profit = price - minPrice
+        if price - minPrice > profit:
+            profit = price - minPrice
     
     return profit
